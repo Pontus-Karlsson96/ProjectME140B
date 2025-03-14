b10a8fb1a35df59e6fc0f079a083b32fb3d9e67d
Pontus-Karlsson96 <143487143+Pontus-Karlsson96@users.noreply.github.com>
2025-03-14 18:08:25 +0100

    test image/audio
    early test image audio limits with gh pages hosting

--- index.js
+++ index.js
@@ -27,26 +27,26 @@ function getLocation() {
   
     if (distance <= malmo) {
       // Användaren är inom tolerance, visa information
-      document.getElementById("main").innerHTML = 
-      `<h1>Malmö</h1>
+      const main = document.getElementById("main");
+      main.innerHTML = 
+      `<h1>${locationObject.limhamn1.title}</h1>
       <h2>Din plats: ${userLat} ${userLon}</h2>
       <h3>Du är ${distance} meter från mig</h3>
       <h4>Body</h4>
-      <article>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-          industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-          survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-          release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-          Ipsum. <br><br>
+      <article>${locationObject.limhamn1.description}
+      </article> 
+      ${locationObject.limhamn1.image}
+        `;
 
-          <b>Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum </b> <br><br>
+      const audio = document.createElement("audio");
+      audio.controls = true;
+      audio.autoplay = true;
+      const source = document.createElement('source');
+      source.src = locationObject.limhamn1.audio;
+      source.type = "audio/mpeg";
+      audio.appendChild(source);
+      main.appendChild(audio);
 
-          <i>Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum</i>
-      </article> `;
     } else if (distance <= sweden) {
       document.getElementById("main").innerHTML = 
       `<h1>Sverige</h1>
@@ -67,10 +67,14 @@ function getLocation() {
 
           <i>Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
               like Aldus PageMaker including versions of Lorem Ipsum</i>
-      </article> `;
+      </article> 
+      
+      `
+      ;
+      
     }
     else {
-      document.getElementById("main").innerText = `Jag vet inte vart du är. ${userLat} ${userLon} ${distance}`;
+      document.getElementById("main").innerText = `Ta dig till en av platserna på kartan. ${userLat} ${userLon} ${distance}`;
     }
   }
   
