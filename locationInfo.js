eec2e4bc52aaa0346778e984836c70991db39f16
Pontus-Karlsson96 <143487143+Pontus-Karlsson96@users.noreply.github.com>
2025-03-18 17:58:17 +0100

    location test

--- locationInfo.js
+++ locationInfo.js
@@ -1,8 +1,8 @@
 const locationObject =  {
 
     MALMO:{ 
-    lat: 55.60587,
-    lon: 13.00073,
+    lat: 0,//55.60587,
+    lon: 0,//13.00073,
     tolerance: 10000,
     title: "Limhamn",
     description: "Limhamn, a charming district in Malmö, Sweden, blends coastal tranquility with urban vibrancy. Once an independent town, it retains a distinct character. Its harbor, bustling with sailboats and fishing vessels, offers scenic views of the Öresund strait. Limestone quarries, historically vital, now host recreational areas. Quaint streets are lined with diverse shops and restaurants, reflecting a mix of old-world charm and modern living. Limhamn's proximity to the sea encourages outdoor activities, from leisurely walks along the beach to kite surfing. The area boasts a relaxed atmosphere, attracting families and those seeking a peaceful yet connected lifestyle.",
@@ -12,6 +12,20 @@ const locationObject =  {
         "<img class='image3' src='./media/limhamn3.jpg' alt='Tre personer i finkläder står på ett hustak och blickar ut över Limhamns industriområde'>",
 ],
     audio: "./media/fi.mp3",
+    },
+
+    TEXAS: {
+        lat: 55.60170625333595,
+        lon: 13.001817923103184,
+        tolerance: 100000,
+        title: 'Texas Longhorn',
+        description: "Fan va gött - Anton Lindeborg",
+        images: [
+            "<img class='image1' src='./media/finland.jpg' alt='grattis Anton'>",
+            "<img class='image2' src='./media/finland.jpg' alt='grattis Anton'>",
+            "<img class='image3' src='./media/finland.jpg' alt='grattis Anton'>",
+        ],
+        audio: "./media/fi.mp3",
     }
 
 };
