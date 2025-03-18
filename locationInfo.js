985437517a5e26b15da0f05bfc0fddbd919afdb2
Pontus-Karlsson96 <143487143+Pontus-Karlsson96@users.noreply.github.com>
2025-03-18 16:53:15 +0100

    test

--- locationInfo.js
+++ locationInfo.js
@@ -1,6 +1,9 @@
 const locationObject =  {
 
-    limhamn1:{ 
+    PLATS1:{ 
+    lat: 55.60587,
+    lon: 13.00073,
+    tolerance: 10000,
     title: "Limhamn",
     description: "Limhamn, a charming district in Malmö, Sweden, blends coastal tranquility with urban vibrancy. Once an independent town, it retains a distinct character. Its harbor, bustling with sailboats and fishing vessels, offers scenic views of the Öresund strait. Limestone quarries, historically vital, now host recreational areas. Quaint streets are lined with diverse shops and restaurants, reflecting a mix of old-world charm and modern living. Limhamn's proximity to the sea encourages outdoor activities, from leisurely walks along the beach to kite surfing. The area boasts a relaxed atmosphere, attracting families and those seeking a peaceful yet connected lifestyle.",
     images: [
